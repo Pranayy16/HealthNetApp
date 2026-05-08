658c2a5a4f6109f07703dceeecebfcf8ca05682e
Javvaji Pranay <javvaji.pranay@cognizant.com>
2026-05-08 14:45:01 +0530

    Fixed Bugs and Added folder Structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,49 +1,10 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/guards/auth-guard';
-import { roleGuard } from './core/guards/role-guard';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./components/auth-component/auth-component')
-        .then(m => m.AuthComponent)
-  },
-  {
-    path: 'unauthorized',
-    loadComponent: () =>
-      import('./components/unauthorized/unauthorized')
-        .then(m => m.Unauthorized)
-  },
-  {
-    path: 'dashboard',
-    canActivate: [authGuard],
-    children: [
-      {
-        path: 'doctor',
-        canActivate: [roleGuard],
-        data: { roles: ['DOCTOR'] },
-        loadComponent: () =>
-          import('./components/auth-component/auth-component')
-            .then(m => m.AuthComponent)
-      },
-      {
-        path: 'admin',
-        canActivate: [roleGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-          import('./components/auth-component/auth-component')
-            .then(m => m.AuthComponent)
-      }
-    ]
-  },
-  { path: '**', redirectTo: '/login' }
 import { HomeComponent } from './components/home-component/home-component';
 import { ComplianceComponent } from './components/compliance-component/compliance-component';
 import { AboutComponent } from './components/about-component/about-component';
 import { LoginComponent } from './components/login-component/login-component';
 import { RegisterComponent } from './components/register-component/register-component';
+import { OutbreakComponent } from './components/outbreak-component/outbreak-component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -52,4 +13,5 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'compliance', component: ComplianceComponent},
     { path: 'about', component: AboutComponent},
+    { path: "outbreaks", component: OutbreakComponent }
 ];
